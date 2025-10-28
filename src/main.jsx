37be8f31f46c34cd1b5a9db2159d7099e58b28f6
yamaguchisora1006k <yamaguchisora1006@gmail.com>
2025-10-28 15:28:18 +0900

    Move build files to root

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/mojicount/"}>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/MojiCount/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
